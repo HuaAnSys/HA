8c8bd29a52fbc21a45c0131e76587d57359ab568
Xiao Xiao Yang <dlyangxx@cn.ibm.com>
2016-05-31 17:07:49 +0800

    android tab position

--- www/js/app.js
+++ www/js/app.js
@@ -8,14 +8,12 @@
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function($ionicConfigProvider) {
+  $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-      $ionicConfigProvider.platform.android.tabs.position('bottom');
-
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
